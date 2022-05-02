5eaa89647cea5ea9676b821859113b5511078433
auyin11 <auyin1111@gmail.com>
2022-05-02 23:01:19 +0800

    v1.0.0

--- cfg.py
+++ cfg.py
@@ -3,54 +3,53 @@ import os
 
 class Cfg:
 
-    def __init__(self):
-        train = True
-        seed = 42
+    train = True
+    seed = 42
+    # TODO
+    wandb = False
+    competition = 'PPPM'
+    _wandb_kernel = 'nakama'
+    debug = False
+    apex = True
+    print_freq = 100
+
+    bbbb = 10000000000000000
+
+    class Dir:
         # TODO
-        wandb = False
-        competition = 'PPPM'
-        _wandb_kernel = 'nakama'
-        debug = False
-        apex = True
-        print_freq = 100
+        output = 'output'
+        train_log = os.path.join(output, 'train_predict.log')
 
-        bbbb = 10000000000000000
+        data = 'data'
 
-        class Dir:
-            # TODO
-            output = 'output'
-            train_log = os.path.join(output, 'train_predict.log')
+    class Train:
+        num_workers = 4
+        batch_size = 16
+        scheduler = 'cosine'  # ['linear', 'cosine']
+        batch_scheduler = True
+        num_cycles = 0.5
+        num_warmup_steps = 0
+        epochs = 3
+        max_len = 512
 
-            data = 'data'
+    class CV:
+        n_fold = 4
+        trn_fold = [0, 1, 2, 3]
 
-        class Train:
-            num_workers = 4
-            batch_size = 16
-            scheduler = 'cosine'  # ['linear', 'cosine']
-            batch_scheduler = True
-            num_cycles = 0.5
-            num_warmup_steps = 0
-            epochs = 3
-            max_len = 512
+    class Model:
+        # TODO
+        pretrained_model = "microsoft/deberta-v3-large"
 
-        class CV:
-            n_fold = 4
-            trn_fold = [0, 1, 2, 3]
-
-        class Model:
-            # TODO
-            pretrained_model = "microsoft/deberta-v3-large"
-
-            encoder_lr = 2e-5
-            decoder_lr = 2e-5
-            min_lr = 1e-6
-            eps = 1e-6
-            betas = (0.9, 0.999)
-            fc_dropout = 0.2
-            target_size = 1
-            weight_decay = 0.01
-            gradient_accumulation_steps = 1
-            max_grad_norm = 1000
+        encoder_lr = 2e-5
+        decoder_lr = 2e-5
+        min_lr = 1e-6
+        eps = 1e-6
+        betas = (0.9, 0.999)
+        fc_dropout = 0.2
+        target_size = 1
+        weight_decay = 0.01
+        gradient_accumulation_steps = 1
+        max_grad_norm = 1000
 
 
 if __name__ == '__main__':
