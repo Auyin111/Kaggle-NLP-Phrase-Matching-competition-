2c7c3fc68a1efbb2297c6ca0692d8cc093a89d7c
auyin11 <auyin1111@gmail.com>
2022-06-19 00:06:48 +0800

    add - allow to upload ensemble model and related code improve - use relative path to define source and destination bug fix - create folder if not exist

--- prepare_upload_folder.py
+++ prepare_upload_folder.py
@@ -1,23 +1,26 @@
 import os
 import shutil
 from utils import cp_child_content
+from pathlib import Path
 
 
 if __name__ == '__main__':
 
     # TODO: use reference path
-    dir_source = r"C:\Users\tommy\Desktop\Machine Learning\Kaggle\USPPPM\competition_patent"
-    dir_destination = r"C:\Users\tommy\Desktop\Machine Learning\Kaggle\USPPPM\competition_patent_upload"
+    dir_source = os.getcwd()
+    dir_destination = os.path.join(os.path.dirname(dir_source), 'competition_patent_upload')
 
-    selected_version = "deberta-v3-base ver1" #
+    list_model_version = ['deberta large', 'albert-base-v2', 'deberta-v3-base ver1']
 
     list_cp_content = ['model', 'own_dataset', 'train_predict', 'gen_data',
-                       'cfg.py', 'main.py', 'utils.py', 'kaggle']
-                       # 'paperspace_setup.sh', 'linux_pantent_requirement.txt', 'run_main.sh']
+                       'cfg.py', 'main.py', 'utils.py', 'kaggle',
+                       'ensemble', 'em_main.py', 'em1.0.7',
+                       'paperspace_setup.sh', 'linux_pantent_requirement.txt', 'run_main.sh']
 
+    Path(dir_source).mkdir(parents=True, exist_ok=True)
     shutil.rmtree(dir_destination)
 
     cp_child_content(dir_source, dir_destination, list_cp_content)
-    if selected_version is not None:
-        shutil.copytree(os.path.join(dir_source, 'output', selected_version),
-                        os.path.join(dir_destination, 'output', selected_version))
+    for version in list_model_version:
+        shutil.copytree(os.path.join(dir_source, 'output', version),
+                        os.path.join(dir_destination, 'output', version))
